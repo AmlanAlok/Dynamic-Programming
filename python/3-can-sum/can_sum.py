f0520f25b2e3c19f982ea42df8368ad52d51fa14
Amlan Alok <amlanalok@gmail.com>
2022-05-21 13:06:39 -0400

    added can_sum_memo

--- python/3-can-sum/can_sum.py
+++ python/3-can-sum/can_sum.py
@@ -17,6 +17,25 @@ def can_sum(target_sum, numbers):
     return False
 
 
+def can_sum_memo(target_sum, numbers, memo={}):
+
+    if target_sum in memo:
+        return memo[target_sum]
+    if target_sum == 0:
+        return True
+    if target_sum < 0:
+        return False
+
+    for n in numbers:
+        remainder = target_sum - n
+
+        memo[remainder] = can_sum_memo(remainder, numbers, memo)
+        if memo[remainder]:
+            return True
+
+    return False
+
+
 class MyTestCase(unittest.TestCase):
 
     def test_01(self):
@@ -24,6 +43,8 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, can_sum(7, [2, 3]))
         self.assertEqual(False, can_sum(7, [2, 4]))
 
+    def test_02(self):
+        self.assertEqual(False, can_sum_memo(300, [7, 14]))
 
 
 if __name__ == '__main__':
