519640113786827688aa68061ec24bac7cf5c936
Amlan Alok <amlanalok@gmail.com>
2022-06-01 19:17:13 -0500

    added how_sum

--- python/4-how-sum/how_sum.py
+++ python/4-how-sum/how_sum.py
@@ -22,8 +22,8 @@ def how_sum(target_sum, numbers):
 class MyTestCase(unittest.TestCase):
 
     def test_01(self):
-        # self.assertEqual([4, 3], how_sum(7, [5, 3, 4, 7]))
-        # self.assertEqual(None, how_sum(7, [2, 3]))
+        self.assertEqual([4, 3], how_sum(7, [5, 3, 4, 7]))
+        self.assertEqual([3, 2, 2], how_sum(7, [2, 3]))
         self.assertEqual([3, 2], how_sum(5, [2, 3]))
         self.assertEqual(None, how_sum(7, [2, 4]))
         # print(how_sum(5, [2, 3]))
