e1dc856209bc2f580b7cc714005255c9e4072963
Amlan Alok <amlanalok@gmail.com>
2022-07-18 19:24:10 -0500

    bfs min added

--- python/2-Trees/1-Basics/4_tree_min.py
+++ python/2-Trees/1-Basics/4_tree_min.py
@@ -30,15 +30,41 @@ def create_num_tree():
 def dfs_min(node):
     if node is None:
         return sys.maxsize
-
     return min(node.val, min(dfs_min(node.left), dfs_min(node.right)))
+
+
+def bfs_min(node):
+    if node is None:
+        return sys.maxsize
+
+    from collections import deque
+    q = deque()
+    q.append(node)
+
+    m = sys.maxsize
+
+    while len(q) > 0:
+        fn = q.popleft()
+
+        m = min(m, fn.val)
+
+        if fn.left:
+            q.append(fn.left)
+        if fn.right:
+            q.append(fn.right)
+
+    return m
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_something(self):
+    def test_dfs_min(self):
         root = create_num_tree()
         self.assertEqual(1, dfs_min(root))
+
+    def test_bfs_min(self):
+        root = create_num_tree()
+        self.assertEqual(1, bfs_min(root))
 
 
 if __name__ == '__main__':
