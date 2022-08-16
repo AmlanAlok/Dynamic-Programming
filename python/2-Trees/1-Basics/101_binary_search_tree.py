0e3281a73f2f2971ef682899bce2de1d0cf6ac43
Amlan Alok <amlanalok@gmail.com>
2022-08-16 03:48:24 -0500

    added binary search tree

--- python/2-Trees/1-Basics/101_binary_search_tree.py
+++ python/2-Trees/1-Basics/101_binary_search_tree.py
@@ -2,6 +2,18 @@ import unittest
 from Node import *
 
 
+def search(root, val):
+
+    if root is None:
+        return False
+    elif val == root.val:
+        return True
+    elif val < root.val:
+        return search(root.left, val)
+    elif val > root.val:
+        return search(root.right, val)
+
+
 def insert(root, val):
 
     if root is None:
@@ -56,7 +68,7 @@ def post_order(root):
 
 class MyTestCase(unittest.TestCase):
 
-    '''
+    """
            8
          /   \
         3     10
@@ -64,8 +76,8 @@ class MyTestCase(unittest.TestCase):
       1   6      14
          / \
         4   7
-    '''
-    def test_something(self):
+    """
+    def test_traversals(self):
         root = None
         root = insert(root, 8)
         root = insert(root, 3)
@@ -80,6 +92,25 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder_2(root))
         self.assertEqual([8, 3, 1, 6, 4, 7, 10, 14], pre_order(root))
         self.assertEqual([1, 4, 7, 6, 3, 14, 10, 8], post_order(root))
+
+    def test_search(self):
+        root = None
+        root = insert(root, 8)
+        root = insert(root, 3)
+        root = insert(root, 1)
+        root = insert(root, 6)
+        root = insert(root, 7)
+        root = insert(root, 10)
+        root = insert(root, 14)
+        root = insert(root, 4)
+
+        inorder_nums = [1, 3, 4, 6, 7, 8, 10, 14]
+        for i in inorder_nums:
+            self.assertEqual(True, search(root, i))
+
+        not_in_nums = [2, 5, 9, 11, 12, 13, 15]
+        for i in not_in_nums:
+            self.assertEqual(False, search(root, i))
 
 
 if __name__ == '__main__':
