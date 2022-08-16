fad792b4315d1e591e6dd1464c243e72d8166d7a
Amlan Alok <amlanalok@gmail.com>
2022-08-16 03:12:04 -0500

    added preorder, inorder and postorder traversal

--- python/2-Trees/1-Basics/101_binary_search_tree.py
+++ python/2-Trees/1-Basics/101_binary_search_tree.py
@@ -18,17 +18,6 @@ def insert(root, val):
 
 def inorder(root):
 
-    # p = []
-    #
-    # if root.left:
-    #     p += inorder(root.left)
-    # if root:
-    #     p += [root.val]
-    # if root.right:
-    #     p += inorder(root.right)
-    #
-    # return p
-
     if root is None:
         return []
 
@@ -49,8 +38,33 @@ def inorder_2(root):
     return p
 
 
+def pre_order(root):
+
+    if root is None:
+        return []
+
+    return [root.val] + pre_order(root.left) + pre_order(root.right)
+
+
+def post_order(root):
+
+    if root is None:
+        return []
+
+    return post_order(root.left) + post_order(root.right) + [root.val]
+
+
 class MyTestCase(unittest.TestCase):
 
+    '''
+           8
+         /   \
+        3     10
+       / \      \
+      1   6      14
+         / \
+        4   7
+    '''
     def test_something(self):
         root = None
         root = insert(root, 8)
@@ -62,10 +76,10 @@ class MyTestCase(unittest.TestCase):
         root = insert(root, 14)
         root = insert(root, 4)
 
-        print(inorder(root))
-
         self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder(root))
         self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder_2(root))
+        self.assertEqual([8, 3, 1, 6, 4, 7, 10, 14], pre_order(root))
+        self.assertEqual([1, 4, 7, 6, 3, 14, 10, 8], post_order(root))
 
 
 if __name__ == '__main__':
