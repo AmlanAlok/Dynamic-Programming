337facf2a8af07d5bafde2c9372e3914aa51c1b3
Amlan Alok <amlanalok@gmail.com>
2022-08-16 21:22:57 -0500

    added setUp and tearDown methods

--- python/2-Trees/1-Basics/101_binary_search_tree.py
+++ python/2-Trees/1-Basics/101_binary_search_tree.py
@@ -28,6 +28,29 @@ def insert(root, val):
     return root
 
 
+def deletion(root, val):
+
+    if root is None:
+        return root
+    elif val == root.val:
+        if root.left is None and root.right is None:
+            root = None
+        elif root.left is not None:
+            root.val = root.left.val
+            root.left = None
+        elif root.right is not None:
+            root.val = root.right.val
+            root.right = None
+        elif root.left is not None and root.right is not None:
+            print('two child scenario')
+    elif val < root.val:
+        root.left = deletion(root.left, val)
+    elif val > root.val:
+        root.right = deletion(root.right, val)
+
+    return root
+
+
 def inorder(root):
 
     if root is None:
@@ -77,21 +100,25 @@ class MyTestCase(unittest.TestCase):
          / \
         4   7
     """
-    def test_traversals(self):
-        root = None
-        root = insert(root, 8)
-        root = insert(root, 3)
-        root = insert(root, 1)
-        root = insert(root, 6)
-        root = insert(root, 7)
-        root = insert(root, 10)
-        root = insert(root, 14)
-        root = insert(root, 4)
+    def setUp(self) -> None:
+        self.root = None
+        self.root = insert(self.root, 8)
+        self.root = insert(self.root, 3)
+        self.root = insert(self.root, 1)
+        self.root = insert(self.root, 6)
+        self.root = insert(self.root, 7)
+        self.root = insert(self.root, 10)
+        self.root = insert(self.root, 14)
+        self.root = insert(self.root, 4)
 
-        self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder(root))
-        self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder_2(root))
-        self.assertEqual([8, 3, 1, 6, 4, 7, 10, 14], pre_order(root))
-        self.assertEqual([1, 4, 7, 6, 3, 14, 10, 8], post_order(root))
+    def tearDown(self) -> None:
+        del self.root
+    
+    def test_traversals(self):
+        self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder(self.root))
+        self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder_2(self.root))
+        self.assertEqual([8, 3, 1, 6, 4, 7, 10, 14], pre_order(self.root))
+        self.assertEqual([1, 4, 7, 6, 3, 14, 10, 8], post_order(self.root))
 
     def test_search(self):
         root = None
@@ -111,6 +138,28 @@ class MyTestCase(unittest.TestCase):
         not_in_nums = [2, 5, 9, 11, 12, 13, 15]
         for i in not_in_nums:
             self.assertEqual(False, search(root, i))
+
+    def test_deletion(self):
+        root = None
+        root = insert(root, 8)
+        root = insert(root, 3)
+        root = insert(root, 1)
+        root = insert(root, 6)
+        root = insert(root, 7)
+        root = insert(root, 10)
+        root = insert(root, 14)
+        root = insert(root, 4)
+
+        print(inorder(root))
+
+        # x = deletion(root, 10)
+        self.assertEqual([1, 3, 4, 6, 7, 8, 10, 14], inorder(root))
+        self.assertEqual([1, 3, 4, 6, 7, 8, 14], inorder(deletion(root, 10)))
+        self.assertEqual([1, 3, 6, 7, 8, 14], inorder(deletion(root, 4)))
+
+        # print(inorder(x))
+
+
 
 
 if __name__ == '__main__':
